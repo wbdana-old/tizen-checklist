4db0e5912e58fb8e35f63ce29f678525f828a61a
wbdana <william.b.dana@gmail.com>
2018-02-10 15:46:00 -0500

    Initial commit for work on checklist app

---
+++ app.js
@@ -0,0 +1,29 @@
+window.onload = function () {
+	window.addEventListener("tizenhwkey", function (ev) {
+		var activePopup = null,
+			page = null,
+			pageid = "";
+
+		if (ev.keyName === "back") {
+			activePopup = document.querySelector(".ui-popup-active");
+			page = document.getElementsByClassName("ui-page-active")[0];
+			pageid = page ? page.id : "";
+
+			if (pageid === "main" && !activePopup) {
+				try {
+					tizen.application.getCurrentApplication().exit();
+				} catch (ignore) {
+				}
+			} else {
+				window.history.back();
+			}
+		}
+	});
+	
+	var listHeader = document.querySelector("#list-header");
+	var listBody = document.querySelector("#list-item");
+	
+	listHeader.addEventListener("click", function() {
+		listBody.innerHTML = "Success";
+	});
+};
