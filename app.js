89a58672ace0108fae132c48f2b1c6050f53ddb5
wbdana <william.b.dana@gmail.com>
2018-02-12 16:46:39 -0500

    Dynamically create checkboxes

--- app.js
+++ app.js
@@ -22,8 +22,34 @@ window.onload = function () {
 	
 	var listHeader = document.querySelector("#list-header");
 	var listBody = document.querySelector("#list-item");
+	var checklist = document.querySelector("#checklist");
+	
+	var sampleItems = ["cheddar", "beef"];
+	
+	function createLi(itemText) {
+		var checkbox = document.createElement('input');
+		checkbox.name = "name";
+		checkbox.type = "checkbox";
+		checkbox.value = "value";
+		checkbox.id = "id";
+		
+		var label = document.createElement('label');
+		label.htmlFor = "id";
+		label.appendChild(document.createTextNode(itemText));
+		
+		var checklist = document.querySelector(".checklist");
+		
+		var li = document.createElement('li');
+		li.appendChild(checkbox);
+		li.appendChild(document.createTextNode(itemText));
+		
+		checklist.appendChild(li);
+	}
 	
 	listHeader.addEventListener("click", function() {
-		listBody.innerHTML = "Success";
+		sampleItems.forEach((sampleItem) => {
+			console.log("Hit", sampleItem);
+			createLi(sampleItem);
+		});
 	});
 };
